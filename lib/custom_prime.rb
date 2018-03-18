ec7c2134ca7bba9ef68ee620e39d9a4d40960a92
Catalin Ilinca <c@talin.ro>
2018-03-18 04:40:41 +0200

    Implement #smallest_divisor and #is_prime? for CustomPrime

--- lib/custom_prime.rb
+++ lib/custom_prime.rb
@@ -1,8 +1,22 @@
 module CustomPrime
   def smallest_divisor
+    # This will return smallest divisor, greater than 1, for a Integer greater than 1
+    int_value = self.to_i
+    raise Exception.new('Number not greater than 1') if int_value <= 1
+    (2..int_value - 1).each do |divisor|
+      if int_value % divisor == 0
+        return divisor
+        break
+      end
+    end
+    nil
   end
 
   def is_prime?
+    int_value = self.to_i
+    # Stop going further if not an Integer greater than 1
+    return false if int_value <= 1
+    return int_value.smallest_divisor ? false : true
   end
 end
 
