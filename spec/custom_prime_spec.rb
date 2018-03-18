bfc732ce83917309912ca443eb22eddb83775882
Catalin Ilinca <c@talin.ro>
2018-03-18 04:38:58 +0200

    Specs for CustomPrime methods

--- spec/custom_prime_spec.rb
+++ spec/custom_prime_spec.rb
@@ -10,8 +10,26 @@ describe CustomPrime do
   end
 
   describe '#smallest_divisor' do
+    it 'raise error if number not greater than 1' do
+      expect { 1.smallest_divisor }.to raise_error(Exception).with_message('Number not greater than 1')
+    end
+
+    it 'returns nil for a number that has no other divisors other than 1 and self' do
+      expect(7.smallest_divisor).to be_nil
+    end
+
+    it 'returns smallest divisor for a number that has other divisor than 1 and self' do
+      expect(105.smallest_divisor).to eq(3)
+    end
   end
 
   describe '#is_prime?' do
+    it 'returns true for a prime number' do
+      expect(19.is_prime?).to be_truthy
+    end
+
+    it 'returns false for not a prime number' do
+      expect(422.is_prime?).to be_falsey
+    end
   end
 end
